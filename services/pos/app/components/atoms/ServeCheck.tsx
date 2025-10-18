de2c64448994475d4e0c2d7a343319788f8ecbe3
takum1 <takum1.k@coins.tsukuba.ac.jp>
2025-10-19 00:34:02 +0900

    Hicheck から Facheckに変更

--- services/pos/app/components/atoms/ServeCheck.tsx
+++ services/pos/app/components/atoms/ServeCheck.tsx
@@ -1,5 +1,5 @@
 import type { OrderEntity, WithId } from "@cafeore/common";
-import { HiCheck } from "react-icons/hi2";
+import { FaCheck } from "react-icons/fa";
 import { cn } from "~/lib/utils";
 import { Button } from "../ui/button";
 
@@ -14,7 +14,7 @@ export const ServeCheck = ({ order, onServe }: props) => {
       onClick={() => onServe(order)}
       className="items-center flex h-16 w-20 flex-col hover:bg-green-200 bg-green-600"
     >
-      <HiCheck className="h-7 w-7 fill-white" strokeWidth={3} />
+      <FaCheck className="h-7 w-7 fill-white" strokeWidth={1.5} />
       <span className="text-xs text-white">
         提供
       </span>
