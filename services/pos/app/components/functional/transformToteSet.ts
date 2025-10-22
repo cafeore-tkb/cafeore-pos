ed9704ee7227248630e4ce6bf92565ae28dfac44
takum1 <takum1.k@coins.tsukuba.ac.jp>
2025-10-22 20:52:27 +0900

    トートセットの注文数分、ゆかりが入るようにした

--- services/pos/app/components/functional/transformToteSet.ts
+++ services/pos/app/components/functional/transformToteSet.ts
@@ -1,12 +1,15 @@
 import { ITEM_MASTER, ItemEntity, type OrderEntity } from "@cafeore/common";
+import { count } from "firebase/firestore";
 
 export function transformToteSet(order: OrderEntity): OrderEntity {
   const yusho = ITEM_MASTER["-"];
   const toteSet = ITEM_MASTER["@"];
-  if (order.items.find((item) => item.id === toteSet.id)) {
+  let toteSetCount = order.items.filter((item) => item.id === toteSet.id).length;
+  while (toteSetCount > 0) {
     order.items.push(
       ItemEntity.fromItem({ ...yusho, price: 0, assignee: null }),
     );
+    toteSetCount--;
   }
   console.dir(order);
   return order;
