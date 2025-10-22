fe8837d6a270bba16900ae443f48793340e578d4
shoga <yayoiclimbing@gmail.com>
2025-10-23 00:47:50 +0900

    dashboardの注文内容リストをコンポーネント化

--- services/pos/app/routes/_header.dashboard.tsx
+++ services/pos/app/routes/_header.dashboard.tsx
@@ -9,17 +9,8 @@ import { orderBy } from "firebase/firestore";
 import { useState } from "react";
 import useSWRSubscription from "swr/subscription";
 import { ItemBarChart } from "~/components/organisms/ItemBarChart";
+import { OrderList } from "~/components/organisms/OrderList";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/components/ui/table";
 import { cn } from "~/lib/utils";
 
 export const meta: MetaFunction = () => {
@@ -47,44 +38,7 @@ export default function Dashboard() {
           <h1 className="text-3xl">注文状況</h1>
           <p>提供待ちオーダー数：{unseved}</p>
         </div>
-        <div>
-          <Table>
-            <TableCaption />
-            <TableHeader
-              className={cn("sticky top-0 z-10 bg-background [&_tr]:border-b")}
-            >
-              <TableRow>
-                <TableHead>No.</TableHead>
-                <TableHead>杯数</TableHead>
-                <TableHead>合計額</TableHead>
-                <TableHead>受付時刻</TableHead>
-                <TableHead>提供時刻</TableHead>
-                <TableHead>時間</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {orders?.map((order) => (
-                <TableRow
-                  className={cn(pass15Minutes(order) === true && "bg-red-300")}
-                  key={order.orderId}
-                  onClick={() => setFocusedOrderId(order.orderId)}
-                >
-                  <TableCell className="font-medium">{order.orderId}</TableCell>
-                  <TableCell>{numOfCups(order)}</TableCell>
-                  <TableCell>￥{order.total}</TableCell>
-                  <TableCell>{order.createdAt.toLocaleTimeString()}</TableCell>
-                  <TableCell>
-                    {order.servedAt == null
-                      ? "未提供"
-                      : order.servedAt?.toLocaleTimeString()}
-                  </TableCell>
-                  <TableCell>{diffTime(order)}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter />
-          </Table>
-        </div>
+        <OrderList orders={orders} onOrderClick={setFocusedOrderId} />
       </div>
       <div className="w-1/2">
         <div className="sticky top-0">
@@ -147,22 +101,9 @@ export default function Dashboard() {
   );
 }
 
-const numOfCups = (order: OrderEntity): number => {
-  return order.items.length;
-};
-
 const diffTime = (order: OrderEntity) => {
   if (order.servedAt == null) return "未提供";
   return dayjs(dayjs(order.servedAt).diff(dayjs(order.createdAt))).format(
     "m:ss",
   );
-};
-
-const pass15Minutes = (order: OrderEntity) => {
-  if (order.servedAt === null)
-    return dayjs(dayjs().diff(dayjs(order.createdAt))).minute() >= 15;
-  if (order.servedAt !== null)
-    return (
-      dayjs(dayjs(order.servedAt).diff(dayjs(order.createdAt))).minute() >= 15
-    );
 };
