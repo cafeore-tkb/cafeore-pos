0e58df41e98adf692490a3c9ef1453a3a1b2a879
takum1 <takum1.k@coins.tsukuba.ac.jp>
2025-10-22 20:55:51 +0900

    for文に修正、謎のcountを削除

--- services/pos/app/components/functional/transformToteSet.ts
+++ services/pos/app/components/functional/transformToteSet.ts
@@ -1,15 +1,13 @@
 import { ITEM_MASTER, ItemEntity, type OrderEntity } from "@cafeore/common";
-import { count } from "firebase/firestore";
 
 export function transformToteSet(order: OrderEntity): OrderEntity {
   const yusho = ITEM_MASTER["-"];
   const toteSet = ITEM_MASTER["@"];
-  let toteSetCount = order.items.filter((item) => item.id === toteSet.id).length;
-  while (toteSetCount > 0) {
+  const toteSetCount = order.items.filter((item) => item.id === toteSet.id).length;
+  for (let i = 0; i < toteSetCount; i++) {
     order.items.push(
       ItemEntity.fromItem({ ...yusho, price: 0, assignee: null }),
     );
-    toteSetCount--;
   }
   console.dir(order);
   return order;
