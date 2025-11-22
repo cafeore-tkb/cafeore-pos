57bf7b070002a15d922f74434b84a32ad4367e35
takum1 <takum1.k@coins.tsukuba.ac.jp>
2025-11-23 02:02:58 +0900

    fix(common): React Router v7対応 - custom-loader.ts
    - @remix-run/reactからreact-routerにインポート元を変更

--- modules/common/src/lib/custom-loader.ts
+++ modules/common/src/lib/custom-loader.ts
@@ -1,4 +1,4 @@
-import { type ClientLoaderFunction, useLoaderData } from "@remix-run/react";
+import { type ClientLoaderFunction, useLoaderData } from "react-router";
 
 /**
  * clientLoader のデータを JSON にシリアライズせずに直接取得する関数
